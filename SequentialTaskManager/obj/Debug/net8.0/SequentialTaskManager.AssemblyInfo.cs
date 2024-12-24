098baf8dcdbece8e28837ef28a795599474068df
semihzivali <zivalisemih@gmail.com>
2024-12-24 22:58:19 +0300

    deneme

--- SequentialTaskManager/obj/Debug/net8.0/SequentialTaskManager.AssemblyInfo.cs
+++ SequentialTaskManager/obj/Debug/net8.0/SequentialTaskManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SequentialTaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39c592048f80b80a2952b0e45d114635d914f168")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a313b67eddae7b8ada38b7f4fa3d117f1b2943d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SequentialTaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SequentialTaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
